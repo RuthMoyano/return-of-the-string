fac0630150b6134289a3007a07df9d57f103a410
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-02-18 13:36:15 -0600

    [Add all changes]

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 /*******************
  * YOUR CODE HERE! *
  *******************/
+function yell(str) {
+  return str + '!';
+}
 
+function getFirstCharacter(str) {
+  return str[0]
+}
+function getLastCharacter(str) {
+  return str[str.length - 1]
+}
+function getOneCharacter(str, i) {
+  return str[i];
+}
 
+function getTwoCharacters(str,index1, index2) {
+  return str[index1] + str[index2];
+}
 
+function makeCapitalized(str) {
+  return str.toUpperCase();
+}
 
+function yellLouder(str) {
+  return str.toUpperCase() + '!!!';
+}
 
+function yellEvenLouder(str) {
+  return makeCapitalized(str) + '!!!';
+}
+function getInitials(str) {
+  const initials1 = str[0];
+  const spaceIndex = str.indexOf(' ');
+  const initials2 = str[spaceIndex + 1];
+
+  return initials1 + '.' + initials2 + '.';
+}
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
